e005605f603c7e6bf8a9e56019d7ddba8bbc6ce0
blackbyte9 <blackbyte9@gmx.net>
2017-02-15 14:29:52 +0100

    add dummie content to activities

--- app/src/main/java/com/abraxy/abxtest/InvActivity.java
+++ app/src/main/java/com/abraxy/abxtest/InvActivity.java
@@ -1,10 +1,8 @@
 package com.abraxy.abxtest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
-import android.widget.TabHost;
 
 public class InvActivity extends AppCompatActivity {
 
@@ -13,24 +11,5 @@ public class InvActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_inv);
-
-        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
-
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("Fighter Tab");
-        TabHost.TabSpec tab2 = tabHost.newTabSpec("Resources Tab");
-        TabHost.TabSpec tab3 = tabHost.newTabSpec("Tools Tab");
-
-        tab1.setIndicator("Fighter");
-        tab1.setContent(new Intent(this, InvActivityFighterTab.class));
-
-        tab2.setIndicator("Resources");
-        tab2.setContent(new Intent(this, InvActivityResourcesTab.class));
-
-        tab3.setIndicator("Tools");
-        tab3.setContent(new Intent(this, InvActivityToolsTab.class));
-
-        tabHost.addTab(tab1);
-        tabHost.addTab(tab2);
-        tabHost.addTab(tab3);
     }
 }
