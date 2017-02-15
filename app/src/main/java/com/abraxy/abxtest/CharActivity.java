f8cc304bfd3b93877e927acfda4dd73888321a07
blackbyte9 <blackbyte9@gmx.net>
2017-02-15 14:36:24 +0100

    add verbose logs to the existing functions

--- app/src/main/java/com/abraxy/abxtest/CharActivity.java
+++ app/src/main/java/com/abraxy/abxtest/CharActivity.java
@@ -2,14 +2,14 @@ package com.abraxy.abxtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import android.util.Log;
 
 public class CharActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Log.v("CharActivity", "onCreate");
         setContentView(R.layout.activity_char);
     }
 }
