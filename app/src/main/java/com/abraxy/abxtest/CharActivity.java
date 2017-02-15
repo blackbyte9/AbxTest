eb10eb14ef968a7af4a7b8139cdd25a6ba0c03a0
blackbyte9 <blackbyte9@gmx.net>
2017-02-15 14:40:27 +0100

    add onClick functions to the buttons on the char activity (with log messages

--- app/src/main/java/com/abraxy/abxtest/CharActivity.java
+++ app/src/main/java/com/abraxy/abxtest/CharActivity.java
@@ -3,6 +3,7 @@ package com.abraxy.abxtest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 public class CharActivity extends AppCompatActivity {
 
@@ -11,5 +12,21 @@ public class CharActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.v("CharActivity", "onCreate");
         setContentView(R.layout.activity_char);
+    }
+
+    public void onProfileButton(View view) {
+        Log.v("CharActivity", "onProfileButton");
+    }
+
+    public void onFighterButton(View view) {
+        Log.v("CharActivity", "onFighterButton");
+    }
+
+    public void onSettingsButton(View view) {
+        Log.v("CharActivity", "onSettingsButton");
+    }
+
+    public void onHelpButton(View view) {
+        Log.v("CharActivity", "onHelpButton");
     }
 }
