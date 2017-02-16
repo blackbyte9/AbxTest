22baa7f8859c446723e0b2bad810200d3b4d86bd
blackbyte9 <blackbyte9@gmx.net>
2017-02-16 10:50:08 +0100

    add Button to backFragment add handler to button make handler change to mapsActivity
    add fragment to CharActivity

--- app/src/main/java/com/abraxy/abxtest/BackButtonFragment.java
+++ app/src/main/java/com/abraxy/abxtest/BackButtonFragment.java
@@ -1,17 +1,20 @@
 package com.abraxy.abxtest;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BackButtonFragment extends Fragment {
+public class BackButtonFragment extends Fragment implements View.OnClickListener {
 
 
     public BackButtonFragment() {
@@ -22,9 +25,18 @@ public class BackButtonFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        Log.v("BackButtonFragment", "onCreateView");
+        Button myButton = new Button(getContext());
+        myButton.setText(R.string.backButton_text);
+        myButton.setOnClickListener(this);
+        myButton.setBackgroundColor(Color.RED);
+        return myButton;
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.v("BackButtonFragment", "onClick");
+        Intent intent = new Intent(getContext(), MapsActivity.class);
+        startActivity(intent);
+    }
 }
