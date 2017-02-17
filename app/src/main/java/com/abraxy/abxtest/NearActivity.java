c056cd3d674538607feac9095dc124f51a4118df
blackbyte9 <blackbyte9@gmx.net>
2017-02-17 10:31:54 +0100

    show a string list on near activity

--- app/src/main/java/com/abraxy/abxtest/NearActivity.java
+++ app/src/main/java/com/abraxy/abxtest/NearActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NearActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,36 @@ public class NearActivity extends AppCompatActivity {
         Log.v("NearActivity", "onCreate");
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_near);
+
+        mListView = (ListView) findViewById(R.id.nearListView);
+
+        updateList();
+    }
+
+
+    protected List<String> getList() {
+        List<String> ret = new ArrayList<>();
+        ret.add("Erster Eintrag");
+        ret.add("Zweiter Eintrag");
+        ret.add("Dritter Eintrag");
+        ret.add("Vierter Eintrag");
+        ret.add("Fünfter Eintrag");
+        ret.add("Erster Eintrag");
+        ret.add("Zweiter Eintrag");
+        ret.add("Dritter Eintrag");
+        ret.add("Vierter Eintrag");
+        ret.add("Fünfter Eintrag");
+        ret.add("Erster Eintrag");
+        ret.add("Zweiter Eintrag");
+        ret.add("Dritter Eintrag");
+        ret.add("Vierter Eintrag");
+        ret.add("Fünfter Eintrag");
+
+        return ret;
+    }
+
+    protected void updateList() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getList());
+        mListView.setAdapter(adapter);
     }
 }
